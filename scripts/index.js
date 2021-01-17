1eb4392a51f9072c4d5b2dacd6a38db0b20ad310
Amardeep Kishore <akishore@salesforce.com>
2021-01-17 19:47:13 +0530

    Added SignUp modal JS

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+//redirect to postslist page
+function allpostspage(){
+  window.location.href = "html/postslist.html";
+}
+
+function closesignin(){
+    var x = document.getElementById("SignInModal");
+    x.close();
+}
